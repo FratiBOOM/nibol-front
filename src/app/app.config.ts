fc50f68471dd91adfc5084ec97f74822cbc8a079
FratiBOOM <ef.enryfrat@gmail.com>
2025-05-27 16:49:46 +0200

    cambiata cartella translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,7 +9,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, '../assets/translate/', '.json');
 }
 
 
